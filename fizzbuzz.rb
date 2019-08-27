dd4e8d536a0a9026c983876fd5e74f7783d2a8e3
christian williams <christian.williamsj@gmail.com>
2019-08-27 19:53:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
+ef fizzbuzz(int)
   if int % 3 == 0 and int % 5 == 0
-    return "Fizzbuzz"
-  elsif int % 3 == 0
-    return "Fizz"
+    return "FizzBuzz"
   elsif int % 5 == 0
-    return "buzz"
-end
+    return "Buzz"
+  elsif int % 3 == 0
+    return  "Fizz"
+
+   end
+
+ end
