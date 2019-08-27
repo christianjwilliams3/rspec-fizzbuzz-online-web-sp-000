b0b2b644e13c97e3638c08c444dd8545830977a1
christian williams <christian.williamsj@gmail.com>
2019-08-27 19:58:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(int)
   if int % 3 == 0 and int % 5 ==0 
     returns "FizzBuzz"
   elsif int % 5 == 0 
-    returns "Fizz"
-  elsif int % 3 == 0 
     returns "Buzz"
+  elsif int % 3 == 0 
+    returns "Fizz"
 end
 
 end
